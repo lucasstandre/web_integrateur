08d082ff38c3a44c2b8daab263291095fefe4fbd
jegau <202269051@cegepsherbrooke.qc.ca>
2024-12-10 22:27:35 -0500

    comparaison

--- classe/ServeurManager.php
+++ classe/ServeurManager.php
@@ -3,23 +3,45 @@ require_once './classe/Serveur.php';
 class ServeurManager
 {
     private $_bdd;
-    const SELECT_ALL_SERVEURS = "SELECT server_id, 
-                                        b.name AS marque, s.model, b.lineUp,
-                                        f.name AS formFactor, o.name AS Os, 
-                                        cb.name AS CPU_brand, c.model AS CPU_model, c.baseClockSpeed AS CPU_baseClockSpeed, CPUCount, 
-                                        r.capacity AS RAM_capacity, r.frequency AS RAM_frequency, RAMCount, 
-                                        st.capacity AS storage_capacity, sti.name AS storage_interface, stt.name AS storage_type, storageCount, 
-                                        description, price, imgName, hasGPU
-                                        FROM Serveur AS s
-                                        INNER JOIN ServerBrand AS b ON s.brand_id = b.ServerBrand_id
-                                        INNER JOIN formFactor AS f ON s.formFactor_id = f.formFactor_id
-                                        INNER JOIN OS AS o ON s.OS_id = o.OS_id
-                                        INNER JOIN CPU AS c ON c.CPU_id = c.CPU_id
-                                        INNER JOIN ProcessorBrand as cb ON c.brand_id = cb.ProcessorBrand_id 
-                                        INNER JOIN RAM AS r ON r.RAM_id = r.RAM_id
-                                        INNER JOIN STORAGE as st ON s.storage_id = st.storage_id
-                                        INNER JOIN StorageInterface as sti ON st.interface_id = sti.storageInterface_id
-                                        INNER JOIN StorageType as stt ON st.type_id = stt.storageType_id";
+    const SELECT_ALL_SERVEURS = "SELECT s.server_id, 
+                                        b.name AS marque, 
+                                        s.model, 
+                                        b.lineUp,
+                                        f.name AS formFactor, 
+                                        o.name AS Os, 
+                                        cb.name AS CPU_brand, 
+                                        c.model AS CPU_model, 
+                                        c.baseClockSpeed AS CPU_baseClockSpeed, 
+                                        s.CPUCount, 
+                                        r.capacity AS RAM_capacity, 
+                                        r.frequency AS RAM_frequency, 
+                                        s.RAMCount, 
+                                        st.capacity AS storage_capacity, 
+                                        sti.name AS storage_interface, 
+                                        stt.name AS storage_type, 
+                                        s.storageCount, 
+                                        s.description, 
+                                        s.price, 
+                                        s.imgName,
+                                        s.hasGPU
+                                FROM Serveur AS s
+                                INNER JOIN ServerBrand AS b ON s.brand_id = b.ServerBrand_id
+                                INNER JOIN formFactor AS f ON s.formFactor_id = f.formFactor_id
+                                INNER JOIN OS AS o ON s.OS_id = o.OS_id
+                                INNER JOIN CPU AS c ON s.CPU_id = c.CPU_id
+                                INNER JOIN ProcessorBrand as cb ON c.brand_id = cb.ProcessorBrand_id 
+                                INNER JOIN RAM AS r ON s.RAM_id = r.RAM_id
+                                INNER JOIN STORAGE as st ON s.storage_id = st.Storage_id
+                                LEFT JOIN StorageInterface as sti ON st.interface_id = sti.storageInterface_id
+                                INNER JOIN StorageType as stt ON st.type_id = stt.storageType_id";
+
+
+    const SELECT_ALL_MODELS = "SELECT model FROM serveur ORDER BY model ASC";
+
+    const SELECT_ALL_PRICES= "SELECT price FROM serveur ORDER BY price ASC";
+    const SELECT_ALL_FORMS= "SELECT formFactor_id, name FROM formfactor ORDER BY formFactor_id ASC";
+    const TAILLES_FILTER = "s.formFactor_id = :taille";
+    const PRIX_FILTER = "s.price < :prix";
 
     public function __construct(PDO $bdd)
     {
@@ -33,5 +55,50 @@ class ServeurManager
                 array_push($serveurs, new Serveur($fetchData));
         }
        return $serveurs;
-    }   
+    }  
+    public function selectAllModeles(): array
+    {
+        return $this->_bdd->query(self::SELECT_ALL_MODELS)->fetchAll();
+    }
+    public function selectAllPrices(): array
+    {
+        return $this->_bdd->query(self::SELECT_ALL_PRICES)->fetchAll();
+    }
+    public function selectAllFormFactors(): array
+    {
+        return $this->_bdd->query(self::SELECT_ALL_FORMS)->fetchAll();
+    }
+    public function selectModeles(array $formArray): array
+    {
+        $modeleArray = array();
+        $bindParam = array();
+        $whereClause = '';
+
+        if (isset($formArray['taille']) && $formArray['taille'] != 'Toutes') {
+            $whereClause .= self::TAILLES_FILTER;
+            $bindArray[":taille"] = $formArray['taille'];
+        }
+        if (isset($formArray['prix']) && $formArray['prix'] != 0) {
+            if (!empty($whereClause)) {
+                $whereClause .= " AND ";
+            }
+
+            $whereClause .= self::PRIX_FILTER;
+            $bindArray[":prix"] = $formArray['prix'];
+        }
+        if (empty($whereClause))
+            $dbResult = $this->_bdd->query(self::SELECT_ALL_SERVEURS)->fetchAll();
+        else {
+            $query = $this->_bdd->prepare(self::SELECT_ALL_SERVEURS . ' WHERE ' . $whereClause);
+
+            $query->execute($bindArray);
+
+            $dbResult = $query->fetchAll();
+        }
+
+        foreach ($dbResult as $row) {
+            array_push($modeleArray, new Serveur($row) );
+        }
+        return $modeleArray;
+    }
 };
--- comparaison.php
+++ comparaison.php
@@ -1,34 +1,204 @@
-<?php include_once("inc/header.php"); 
+<?php include_once("inc/header.php");
 $serveurManager = new serveurManager($bdd);
-$serveurs = $serveurManager->getServeur();?>
-    <div class="comparaison">
-    <table class="comparaisonImage">
-        <tr>
-            <td>image1</td>
-            <td>image2</td>
-        </tr>
-    </table>
-    <table class="comparaison">
-        <tr>
-            <th colspan="3">Comparaison</th>
-        </tr>
+$serveurs = $serveurManager->getServeur();
+//if (isset($_REQUEST['comp1']) && isset($_REQUEST['comp1'])) {
+if(1 != 0){
+    $serveur1 = $serveurs[0];
+    $serveur2 = $serveurs[1]; ?>
+    <div id="comparaison">
+        <table id="image">
+            <tr>
+                <td><img src="img/<?= $serveur1->get_imgName() ?>" alt="ok"></td>
+                <td><img src="img/<?= $serveur2->get_imgName() ?>" alt=""></td>
+            </tr>
+        </table>
 
-        <tr>
-            <td><?=$serveurs[0]->get_marque() ?></td>
-            <td>Marque</td>
-            <td><?=$serveurs[1]->get_marque() ?></td>
-        </tr>
-        <tr>
-            <td><?=$serveurs[0]->get_model() ?></td>
-            <td>Modele</td>
-            <td><?=$serveurs[1]->get_model() ?></td>
-        </tr>
-        <tr>
-            <td>1</td>
-            <td>Score</td>
-            <td>2</td>
-        </tr>
+        <table id="marque">
+            <tr>
+                <th colspan="3">Comparaison</th>
+            </tr>
+            <tr>
+                <td><?= $serveur1->get_marque() ?></td>
+                <td><b>Marque</b></td>
+                <td><?= $serveur2->get_marque() ?></td>
+            </tr>
+            <tr>
+                <td><?= $serveur1->get_model() ?></td>
+                <td><b>Modele</b></td>
+                <td><?= $serveur2->get_model() ?></td>
+            </tr>
+            <tr>
+                <td><?= $serveur1->get_lineUp() ?></td>
+                <td><b>Série</b></td>
+                <td><?= $serveur2->get_lineUp() ?></td>
+            </tr>
+            <tr class="couleur">
+                <td>11</td>
+                <td><b>Score</b></td>
+                <td>2</td>
+            </tr>
+        </table>
 
-    </table>
+        <table id="cpu">
+            <tr>
+                <th>2</th>
+                <th>CPU</th>
+                <th>1</th>
+
+            </tr>
+            <tr>
+                <td><?= $serveurs[0]->get_CPU_brand() ?></td>
+                <td><b>Marque</b></td>
+                <td><?= $serveurs[1]->get_CPU_brand() ?></td>
+            </tr>
+            <tr>
+                <td><?= $serveurs[0]->get_CPU_model() ?></td>
+                <td><b>Modele</b></td>
+                <td><?= $serveurs[1]->get_CPU_model() ?></td>
+            </tr>
+            <tr class=" couleur">
+            <td><?= $serveurs[0]->get_CPU_baseClockSpeed() ?></td>
+            <td><b>Base Clock Speed</b></td>
+            <td><?= $serveurs[1]->get_CPU_baseClockSpeed() ?></td>
+            </tr>
+            <tr class="couleur">
+                <td><?= $serveurs[0]->get_CPUCount() ?></td>
+                <td><b>Nombre</b></td>
+                <td><?= $serveurs[1]->get_CPUCount() ?></td>
+            </tr>
+            <tr class="couleur">
+                <td>1</td>
+                <td><b>Score</b></td>
+                <td>2</td>
+            </tr>
+        </table>
+
+        <table id="ram">
+
+            <tr>
+                <th>2</th>
+                <th>RAM</th>
+                <th>1</th>
+
+            </tr>
+            <tr class="couleur">
+                <td><?= $serveurs[0]->get_RAM_capacity() ?></td>
+                <td><b>Capacité</b></td>
+                <td><?= $serveurs[1]->get_RAM_capacity() ?></td>
+            </tr>
+            <tr class="couleur">
+                <td><?= $serveurs[0]->get_RAM_frequency() ?></td>
+                <td><b>Frequence</b></td>
+                <td><?= $serveurs[1]->get_RAM_frequency() ?></td>
+            </tr>
+            <tr class="couleur">
+                <td><?= $serveurs[0]->get_RAMCount() ?></td>
+                <td><b>Nombre</b></td>
+                <td><?= $serveurs[1]->get_RAMCount() ?></td>
+            </tr>
+            <tr class="couleur">
+                <td>1</td>
+                <td><b>Score</b></td>
+                <td>2</td>
+            </tr>
+        </table>
+
+        <table id="stockage">
+
+            <tr>
+                <th>2</th>
+                <th>Stockage</th>
+                <th>1</th>
+
+            </tr>
+            <tr class="couleur">
+                <td><?= $serveurs[0]->get_storage_capacity() ?></td>
+                <td><b>Capacité</b></td>
+                <td><?= $serveurs[1]->get_storage_capacity() ?></td>
+            </tr>
+            <tr>
+                <td><?= $serveurs[0]->get_storage_interface() ?></td>
+                <td><b>interface</b></td>
+                <td><?= $serveurs[1]->get_storage_interface() ?></td>
+            </tr>
+            <tr>
+                <td><?= $serveurs[0]->get_storage_type() ?></td>
+                <td><b>type</b></td>
+                <td><?= $serveurs[1]->get_storage_type() ?></td>
+            </tr>
+            <tr class="couleur">
+                <td><?= $serveurs[0]->get_storageCount() ?></td>
+                <td><b>Nombre</b></td>
+                <td><?= $serveurs[1]->get_storageCount() ?></td>
+            </tr>
+            <tr class="couleur">
+                <td>1</td>
+                <td><b>Score</b></td>
+                <td>2</td>
+            </tr>
+        </table>
     </div>
-<?php include_once("inc/footer.php"); ?>
+    <section id="compButtons">
+        <div>
+            <button><a href="#marque">Score</a></button>
+            <button><a href="#prix">Prix</a></button>
+            <button><a href="#cpu">Cpu</a></button>
+            <button><a href="#ram">Ram</a></button>
+            <button><a href="#stockage">Stockage</a></button>
+            <button><a href="#ram">Autre</a></button>
+        </div>
+        <button id="btn-Couleur">Couleur</button>
+    <?php
+} else { ?>
+        <div class="compVide">
+            <h2>Veuillez selectionner deux produits</h2>
+            <h3>filtre de modèle</h3>
+            <?php $prix = $serveurManager->selectAllPrices()[0][0]
+            ?>
+
+            <form action="comparaison.php" class="filtreComparer">
+                <label for="minimum">Prix minimum:</label>
+                <input type="text" name="prix" id="minimum" value=0>
+                <br>
+                <label for="taille">Taille:</label>
+                <select name="taille" id="taille">
+                    <option value="Toutes" selected>Toutes</option>
+                    <?php
+                    $formArray = $serveurManager->selectAllFormFactors();
+                    foreach ($formArray as $form)
+                        echo '<option value="' . $form[0] . '">' . $form[1] . '</option>';
+                    ?>
+                </select>
+                <input type="hidden" name="action" value="filtreComparer">
+                <button class="button" type="submit">Filtrer</button>
+            </form>
+            <?php $serveurArray = 0;
+
+            if (isset($_REQUEST['action']) && $_REQUEST['action'] == 'filtreComparer') {
+                $serveurArray = $serveurManager->selectModeles($_REQUEST);
+            } else {
+                $serveurArray = $serveurManager->selectAllModeles();
+            } ?>
+            <h3>Selectionner un modèle</h3>
+            <form action="ajouterComparer">
+                <label for="modele">Modèle:</label>
+                <select name="modele" id="modele">
+                    <?php
+                    foreach ($serveurArray as $serveur){
+                        echo '<option value="' . $serveur->get_server_id() . '">' . $serveur->get_model() . '</option>';    
+                    }
+                    ?>
+                </select>
+                <button class="button" type="submit">Ajouter le produit</button>
+            </form>
+            
+        </div>
+        <button hidden="true" id="btn-Couleur">Couleur</button>
+
+    <?php
+}
+    ?>
+    </section>
+
+
+    <?php include_once("inc/footer.php"); ?>
